a1bf82cdb1a05ae1ef9021103f024f75e18b3c73
hadeelghzaleen <121434709+hadeelghzaleen@users.noreply.github.com>
2025-01-17 10:35:00 +0200

    Delete homework6_html and css directory

--- homework6_html and css/http_server.py
+++
@@ -1,8 +0,0 @@
-import http.server
-import socketserver
-
-port=8080
-Handler=http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("",port),Handler)as  http:
-    print("serving at port ", port)
-    http.serve_forever()
